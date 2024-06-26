c80810b5823417b3fe21598f80bab38148586ca1
unknown <kakondey701@gmail.com>
2024-06-26 10:21:25 +0530

    the final logic for string calculator

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,37 @@
 function add(numbers) {
-  if (!numbers) return 0;
-  const numbersArray = numbers.split(',');
+  // Handle empty string
+  if (!numbers.trim()) return 0;
 
-  return numbersArray.reduce((acc, num) => acc + parseInt(num), 0);
+  const delimiterRegex = /^\/\/([^\n]+)/;
+  const match = numbers.match(delimiterRegex);
+  if (match) {
+    const delimiter = match[1]; // Extract the custom delimiter
+
+    const numbersStr = numbers.substring(numbers.indexOf('\n') + 1);
+
+    return add(
+      numbersStr.replace(new RegExp(`${delimiter}`, 'g'), ',')
+    );
+  }
+
+  const numbersArray = numbers.split(/[\s,]+/);
+
+  return numbersArray.reduce((acc, num) => {
+    const parsedNum = parseFloat(num);
+    if (!isNaN(parsedNum)) {
+      if (parsedNum < 0) {
+        const cleanedInput = numbers
+          .replace(/(\d+-?)(?!\d)/g, '$1 ')
+          .replace(/[^0-9-. ]/g, '')
+          .trim();
+        throw new Error(
+          `negative numbers not allowed ${cleanedInput}`
+        );
+      }
+      return acc + parsedNum;
+    }
+    return acc;
+  }, 0);
 }
 
 module.exports = { add };
