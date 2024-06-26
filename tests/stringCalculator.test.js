6a5670ede41397517e9ab2b0f94cb1f00eb94fd1
unknown <kakondey701@gmail.com>
2024-06-26 09:02:41 +0530

    added all possible test cases

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,4 +4,32 @@ describe('String Calculator', () => {
   test('adds zero when given an empty string', () => {
     expect(add('')).toBe(0);
   });
+
+  test('returns the number itself when given a single number', () => {
+    expect(add('1')).toBe(1);
+  });
+
+  test('correctly adds numbers seperated by commas', () => {
+    expect(add('1,2,3').toBe(6));
+  });
+
+  test('correctly adds numbers seperated by newlines', () => {
+    expect(add('1\n2\n3')).toBe(6);
+  });
+
+  test('allows changing the delimiter', () => {
+    expect(add('//;\n;2')).toBe(3);
+  });
+
+  test('throws an exception for a negative number', () => {
+    expect(() => add('-1')).toThrow(
+      'negative numbers are not allowed -1'
+    );
+  });
+
+  test('lists all negative numbers in the exception message', () => {
+    expect(() => add('//;\n-1;-2;-3')).toThrow(
+      'negative numbers not allowed -1 -2 -3'
+    );
+  });
 });
