9fa0297adc2e7d357a763492c9f28e99b3d2fd40
unknown <kakondey701@gmail.com>
2024-06-26 09:14:25 +0530

    added basic calculator

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,3 +1,8 @@
-function add(numbers) {}
+function add(numbers) {
+  if (!numbers) return 0;
+  const numbersArray = numbers.split(',');
+
+  return numbersArray.reduce((acc, num) => acc + parseInt(num), 0);
+}
 
 module.exports = { add };
